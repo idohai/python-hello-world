a9529b6026f115718f8844f167ac0a651d170fd5
idohai <75530198+idohai@users.noreply.github.com>
2025-03-13 11:59:04 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,19 @@
-from http.server import BaseHTTPRequestHandler
+# from http.server import BaseHTTPRequestHandler
 
-class handler(BaseHTTPRequestHandler):
+# class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write(self.path.encode('utf-8'))
-        return
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header('Content-type','text/plain')
+#         self.end_headers()
+#         self.wfile.write(self.path.encode('utf-8'))
+#         return
+from sanic import Sanic
+from sanic.response import json
+app = Sanic()
+ 
+ 
+@app.route('/')
+@app.route('/<path:path>')
+async def index(request, path=""):
+    return json({'hello': path})
